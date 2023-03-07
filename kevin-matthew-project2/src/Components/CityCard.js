97c53f8dfc2f3979a0f84ab571d4c7e7935b1005
Kevin Salinas <kevinsalinas155@gmail.com>
2023-03-07 14:03:40 -0500

    added Links to Card

--- kevin-matthew-project2/src/Components/CityCard.js
+++ kevin-matthew-project2/src/Components/CityCard.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 
-function CityCard ( { name, image } ) {
+
+function CityCard ( { name, image, id } ) {
 
 
     return (
@@ -14,12 +16,12 @@ function CityCard ( { name, image } ) {
       </figure>
       <div class="article-body">
         <h2>{ name }</h2>
-        <a href="#" class="read-more">
+        <Link to={`/cities/${id}`} class="read-more">
           Read more <span class="sr-only">about this is some title</span>
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
           </svg>
-        </a>
+        </Link>
       </div>
     </div>
   </article>
