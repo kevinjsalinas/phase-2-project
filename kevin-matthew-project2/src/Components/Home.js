a4af380b1ec461dc63d42b942427cd62648637af
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-08 14:47:01 -0500

    added css for home page

--- kevin-matthew-project2/src/Components/Home.js
+++ kevin-matthew-project2/src/Components/Home.js
@@ -1,12 +1,27 @@
 import React from "react";
-
+import "../home.css"
 
 
 function Home () {
 
     
     return (
-        <div>Hello Travelers!!!</div>
+        <div><div className="page">
+        <div className="page__container">
+          <a href="#popup-article" className="open-popup">open popup</a>
+        </div>
+      </div>
+      <div id="popup-article" className="popup">
+        <div className="popup__container">
+          <a href="#" class="popup__close">
+            <span className="screen-reader">close</span>
+          </a>  
+          <div className="popup__content">
+            <h1 className="popup__title r-title">Welcome Travelers!!!</h1>
+            <p>Click on any option to get your journey started</p>
+          </div>
+        </div>
+      </div></div>
     )
 }
 
