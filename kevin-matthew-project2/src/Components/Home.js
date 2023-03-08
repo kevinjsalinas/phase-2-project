48957989c26415dd5618aaa136274aa9ef0cacc4
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-08 16:13:00 -0500

    fixed errors

--- kevin-matthew-project2/src/Components/Home.js
+++ kevin-matthew-project2/src/Components/Home.js
@@ -13,14 +13,13 @@ function Home () {
       </div>
       <div id="popup-article" className="popup">
         <div className="popup__container">
-          <a href="#" class="popup__close">
+          <a href="#" className="popup__close">
             <span className="screen-reader">close</span>
           </a>  
           <div className="popup__content">
-          <div class="container">
-            <h1 className="h1home"contenteditable>Welcome Travelers!!!</h1>
+          <div className="container">
+            <h1 className="h1home">Welcome Travelers!!!</h1>
             <p>Click on any option to start your journey</p>
-            
             </div>
           </div>
         </div>
