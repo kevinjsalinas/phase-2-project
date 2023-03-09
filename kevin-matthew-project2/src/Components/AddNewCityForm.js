91671d6272a3f97a77f448d8492cd013b96c2d29
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-09 10:29:20 -0500

    testing search bar

--- kevin-matthew-project2/src/Components/AddNewCityForm.js
+++ kevin-matthew-project2/src/Components/AddNewCityForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import "../form.css"
 
 
 function AddNewCityForm ({AddNewCityToState}) {
@@ -30,21 +31,42 @@ function AddNewCityForm ({AddNewCityToState}) {
 
     }
 
-    
     return (
-        <div className="ui segment">
-            <h2>Can't find your city? Add yours here!</h2>
-            <form onSubmit={handleNewCitySubmit} className="ui form">
-                <div><input onChange={handleAddForm} type="text" name="name" placeholder="city name" /></div>
-                <div><input onChange={handleAddForm} type="text" name="image" placeholder="image URL" /></div>
-                <div><input onChange={handleAddForm} type="text" name="population" placeholder="city population" /></div>
-                <div><input onChange={handleAddForm} type="text" name="description" placeholder="city description" /></div>
-                <div><input onChange={handleAddForm} type="text" name="attractions" placeholder="city attractions" /></div>
-                <button className="thisButton" type="submit">
-                    Add city
-                </button>
-            </form>
+        <form onSubmit={ handleNewCitySubmit } className="travel-form">
+      <h2>Add your favorite city</h2>
+      <div className="form-group">
+      <input onChange={handleAddForm} type="text" name="name" placeholder="city name" required/> 
+      </div>
+      <div className="form-group">
+      <input onChange={handleAddForm} type="text" name="image" placeholder="image URL" required/>
+      </div>
+      <div className="form-group">
+      <input onChange={handleAddForm} type="text" name="population" placeholder="city population" required/>
+      </div>
+      <div className="form-group">
+      <input onChange={handleAddForm} type="text" name="description" placeholder="city description" required/>
+      </div>
+      <div className="form-group">
+      <input onChange={handleAddForm} type="text" name="attractions" placeholder="city attractions" />
+      </div>
+        <div className="form-group">
+        <div className="rate">
+        <input type="radio" id="star5" name="rate" value="5" />
+        <label for="star5" title="text">5 stars</label>
+        <input type="radio" id="star4" name="rate" value="4" />
+        <label for="star4" title="text">4 stars</label>
+        <input type="radio" id="star3" name="rate" value="3" />
+        <label for="star3" title="text">3 stars</label>
+        <input type="radio" id="star2" name="rate" value="2" />
+        <label for="star2" title="text">2 stars</label>
+        <input type="radio" id="star1" name="rate" value="1" />
+        <label for="star1" title="text">1 star</label>
         </div>
+        </div>
+      <div className="form-group">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
     )
 }
 
