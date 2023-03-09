8f99337faeb887efa5ae4b2b3061de81b567347d
Kevin Salinas <kevinsalinas155@gmail.com>
2023-03-09 10:23:57 -0500

    added more semantic css for cities

--- kevin-matthew-project2/src/Components/Home.js
+++ kevin-matthew-project2/src/Components/Home.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "../home.css"
+import CityList from "./CityList";
+import {Link} from "react-router-dom"
 
 
 function Home () {
 
     
-    return (
-        <div><div className="page">
-        <div className="page__container">
-          <a href="#popup-article" className="open-popup">open popup</a>
-        </div>
+return (
+  <div>
+    <div className="page">
+      <div className="page__container">
+       <Link to="/cities" className="open-popup">Start</Link>
       </div>
-      <div id="popup-article" className="popup">
-        <div className="popup__container">
-          <a href="#" className="popup__close">
-            <span className="screen-reader">close</span>
-          </a>  
-          <div className="popup__content">
-          <div className="container">
-            <h1 className="h1home">Welcome Travelers!!!</h1>
-            <p>Click on any option to start your journey</p>
-            </div>
-          </div>
-        </div>
-      </div></div>
-    )
-}
+    </div>
+  <div id="popup-article" className="popup">
+    <div className="popup__container">
+      <a href="#" className="popup__close">
+        <span className="screen-reader">close</span>
+      </a>  
+    <div className="popup__content">
+      <div className="container">
+        {/* <h1 className="h1home">Welcome Travelers!!!</h1>
+        <p>Click on any option to start your journey</p> */}
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+)}
 
 export default Home;
