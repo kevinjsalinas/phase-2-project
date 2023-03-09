8d616d56b06bc235b4e54f3febdc47a540a74d3c
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-09 16:52:06 -0500

    list and font changes

--- kevin-matthew-project2/src/Components/CityCard.js
+++ kevin-matthew-project2/src/Components/CityCard.js
@@ -28,11 +28,11 @@ function CityCard ( { name, image, id, addCityInfoIdToState, setSearchCity } ) {
                 className="read-more">
                 Read more <span className="sr-only">about this is some title</span>
               <svg 
-                xmlns="http://www.w3.org/2000/svg" 
+                xmlns="http://www.w3.org/2000/svg"
                 className="icon" 
                 viewBox="0 0 20 20"
                 fill="currentColor">
-              <path 
+              <path
                 fillRule="evenodd" 
                 d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" 
                 clipRule="evenodd" />
@@ -43,22 +43,6 @@ function CityCard ( { name, image, id, addCityInfoIdToState, setSearchCity } ) {
         </article>
       </section>
     </div>
-  // <div>
-  //   <div class="wrap">
-  //     <div class="box">
-  //       <div class="box-top">
-  //         <img class="box-image" src={ image } />
-  //           <div class="title-flex">
-  //             <h3 class="box-title"> { name } </h3>
-  //           </div>
-  //       </div>
-  //           <NavLink to={ `/cities/info/${id}` } onClick={ () => addCityInfoIdToState(id) } onClick2={ resetSearch } className="button">
-  //           Read More
-  //           </NavLink>
-  // </div>
-  
-  //   </div>
-  // </div>
     )
 }
 
--- kevin-matthew-project2/src/Components/Home.js
+++ kevin-matthew-project2/src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../home.css"
+import { Link } from "react-router-dom";
 
 function Home () {
     
@@ -17,11 +18,15 @@ function Home () {
               <h1 className="h1home">Welcome Travelers!!!</h1>
               <p>As a traveler, there's nothing quite like the excitement of discovering new places and immersing yourself in different cultures.</p>
               <p>This application is here to help you choose what city you want to travel to next!!</p>
-            <div className="home-list">
-              <li>View Cities</li>
-              <li>Add your favorite city</li>
-              <li>Leave a comment </li>
-            </div>  
+            <ul className="list-holder">
+              <Link to="/cities">
+              <li className="home-list">View Cities</li>
+              </Link>
+              <Link to="/cities/new">
+              <li className="home-list">Add your favorite city</li>
+              </Link>
+              <li className="home-list">Leave a comment</li>
+            </ul>  
             </div>
           </div>
         </div>
